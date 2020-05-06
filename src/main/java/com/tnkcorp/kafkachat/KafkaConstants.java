4299d8aeae05388d6bb6cb934e28ea7278c9e1b3
alonzazo <oscar.azofeifa.segura@gmail.com>
2020-05-06 17:59:31 -0600

    first commit

---
+++ src/main/java/com/tnkcorp/kafkachat/KafkaConstants.java
@@ -0,0 +1,7 @@
+package com.tnkcorp.kafkachat;
+
+public class KafkaConstants {
+    public static final String KAFKA_TOPIC = "kafka-chat";
+    public static final String GROUP_ID = "kafka-sandbox";
+    public static final String KAFKA_BROKER = "172.31.54.245:9092";
+}
