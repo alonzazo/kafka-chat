363d6957ba8a6cf2f1514f146d74f5ea2327dc38
alonzazo <oscar.azofeifa.segura@gmail.com>
2020-05-06 18:10:50 -0600

    first commit

--- src/main/java/com/tnkcorp/kafkachat/ChatController.java
+++ src/main/java/com/tnkcorp/kafkachat/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@CrossOrigin(origins = {"http://3.83.105.133:80", "http://3.83.105.133"})
 public class ChatController {
 
     @Autowired
